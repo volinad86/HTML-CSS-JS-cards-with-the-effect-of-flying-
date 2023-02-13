fed07b372e5984a31ed1219cad95fd64c112174d
volinad86git config --global user.name volinad86 <dima_student2005@mail.ru>
2023-02-13 15:42:42 -0500

    Add  duplicate image & func.

--- script.js
+++ script.js
@@ -9,6 +9,49 @@ let imgtodrag = $(this)
 .parent(".card")
 .find("img")
 .eq(0);
+if (imgtodrag) {
+    // duplicate image
+    var imgclone = imgtodrag
+    .clone()
+    .offset({
+        top: imgtodrag.offset().top,
+        left: imgtodrag.offset().left,
+    })
+    .css({
+        opacity: "0.8",
+        position: "absolute",
+        height: "150px",
+        width: "150px",
+        objectFit: "cover",
+        "z-index": "100"
+    })
+    .appendTo($("body"))
+    .animate(
+        {
+        top: cart.offset().top + 20,
+        left: cart.offset().left + 30,
+        width: 75,
+        height: 75
+        },
+        1000,
+        "easeInOutExpo"
+    );
+
+    setTimeout(function () {
+    count++;
+    $(".cart-nav .item-count").text(count);
+    }, 1500);
+
+    imgclone.animate(
+    {
+        width: 0,
+        height: 0
+    },
+    function () {
+        $(this).detach();
+    }
+    );
+    }
 
 
 });
